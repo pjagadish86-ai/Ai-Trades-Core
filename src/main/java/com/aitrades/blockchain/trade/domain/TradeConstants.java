2bb3046d053939deb4b50f81ff2aa62c4698c830
Padimala Jagadesh <22383480+pjagadish86@users.noreply.github.com>
2021-05-01 08:12:43 -0400

    added dynamically exchanges based on blockchain and exchange using

--- src/main/java/com/aitrades/blockchain/trade/domain/TradeConstants.java
+++ src/main/java/com/aitrades/blockchain/trade/domain/TradeConstants.java
@@ -3,26 +3,11 @@ package com.aitrades.blockchain.trade.domain;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.google.common.collect.ImmutableMap;
-
 public final class TradeConstants {
 	
 	public static final String UNISWAP = "UNISWAP";
-	public static final String UNISWAP_FACOTRYADDRESS ="0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f";
-	public static final String UNISWAP_ROUTERADDRESS ="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-	public static final String UNISWAP_WETH_FACOTRYADDRESS ="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
-
-	
 	public static final String PANCAKE = "PANCAKE";
-	public static final String PANCAKE_FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
-	public static final String PANCAKE_ROUTER_ADDRESS = "0x10ed43c718714eb63d5aa57b78b54704e256024e";
-	public static final String PANCAKE_WBNB_FACOTRYADDRESS ="0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
-
 	public static final String SUSHI = "SUSHI";
-	public static final String SUSHI_FACTORY_ADDRESS = "0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac";
-	public static final String SUSHI_ROUTER_ADDRESS = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F";
-	public static final String SUSHI_WETH_FACOTRYADDRESS ="0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
-	
 	public static final String ORDER_DECISION = "ORDER_DECISION";
 
 	public static final String SNIPETRANSACTIONREQUEST = "SNIPETRANSACTIONREQUEST";
@@ -34,12 +19,6 @@ public final class TradeConstants {
 	
 	public static final String OUTPUT_TOKENS = "OUTPUT_TOKENS";
 	public static final String INPUT_TOKENS = "INPUT_TOKENS";
-	
-	public static final Map<String, String> ROUTER_MAP = ImmutableMap.of(UNISWAP, UNISWAP_ROUTERADDRESS, PANCAKE, PANCAKE_ROUTER_ADDRESS, SUSHI, SUSHI_ROUTER_ADDRESS);
-	
-	public static final Map<String, String> FACTORY_MAP = ImmutableMap.of(UNISWAP, UNISWAP_FACOTRYADDRESS, PANCAKE, PANCAKE_FACTORY_ADDRESS, SUSHI, SUSHI_FACTORY_ADDRESS);
-	
-	public static final Map<String, String> WETH_MAP = ImmutableMap.of(UNISWAP, UNISWAP_WETH_FACOTRYADDRESS, PANCAKE, PANCAKE_WBNB_FACOTRYADDRESS, SUSHI, SUSHI_WETH_FACOTRYADDRESS);
 	
 	public static final String ETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 
